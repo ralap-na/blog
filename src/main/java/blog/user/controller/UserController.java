e50e0f140648a0d080eef91be84101ad99e571db
oejk97614 <oejk97614@gmail.com>
2025-04-09 22:04:50 +0800

    Added loginUser in userController and UserService.

--- src/main/java/blog/user/controller/UserController.java
+++ src/main/java/blog/user/controller/UserController.java
@@ -1,13 +1,12 @@
 package blog.user.controller;
 
 import blog.user.service.UserService;
+import jakarta.servlet.http.HttpSession;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -29,4 +28,33 @@ public class UserController {
             return ResponseEntity.internalServerError().body("Failed to create user.");
         }
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> loginUser(@RequestBody String userInfo, HttpSession session) {
+        JSONObject jsonObject = new JSONObject(userInfo);
+        String username = jsonObject.getString("username");
+        String password = jsonObject.getString("password");
+
+        boolean result = userService.loginUser(username, password);
+        if (result) {
+            String userId = userService.getUserIdByUserName(username);
+
+            session.setAttribute("userId", userId);
+
+            return ResponseEntity.ok("User login successfully.");
+        } else {
+            return ResponseEntity.internalServerError().body("Failed to login user.");
+        }
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<String> getProfile(HttpSession session) {
+        String userId = (String) session.getAttribute("userId");
+        if (userId == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Not logged in.");
+        }
+        return ResponseEntity.ok("Logged in as user: " + userId);
+    }
+
+
 }
