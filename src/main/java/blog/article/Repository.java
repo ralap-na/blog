aa492be8247b3e1f5ee8fb5ea7229e013e382cdc
itaouo <158411654+itaouo@users.noreply.github.com>
2025-04-10 22:11:33 +0800

    [Feature] Add Notification Service.

--- src/main/java/blog/article/Repository.java
+++ src/main/java/blog/article/Repository.java
@@ -1,14 +1,17 @@
 package blog.article;
 
+import blog.notification.entity.Notification;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Component
 public class Repository {
 
     private Map<String, Article> articleList = new HashMap<>();
+    private Map<String, Notification> notificationList = new HashMap<>();
 
     public Article findArticleById(String articleId){
         return articleList.get(articleId);
@@ -16,5 +19,23 @@ public class Repository {
 
     public void saveArticle(Article article) {
         articleList.put(article.getArticleId(), article);
+    }
+
+    public Notification findNotificationById(String id) {
+        return notificationList.get(id);
+    }
+
+    public List<Notification> findNotificationsByUserId(String userId) {
+        return notificationList.values().stream()
+                .filter(notification -> notification.getUserId().equals(userId))
+                .toList();
+    }
+
+    public void saveNotification(Notification notification) {
+        notificationList.put(notification.getNotificationId(), notification);
+    }
+
+    public void deleteNotificationById(String notificationId) {
+        notificationList.remove(notificationId);
     }
 }
