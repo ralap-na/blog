d5b60a86d0b74c8bb85864427ccc2105c624fd5b
itaouo <itaouo58@gmail.com>
2025-04-12 19:34:00 +0800

    [Bug fixing] Fix an issue where NotificationController could not request messages.

--- src/main/java/blog/notification/controller/NotificationController.java
+++ src/main/java/blog/notification/controller/NotificationController.java
@@ -15,22 +15,22 @@ public class NotificationController {
     @Autowired
     NotificationService notificationService;
 
-    @GetMapping
-    public List<Notification> getUserNotifications(@RequestBody String userId) {
+    @GetMapping("/user/{userId}")
+    public List<Notification> getUserNotifications(@PathVariable("userId") String userId) {
         return notificationService.getUserNotifications(userId);
     }
 
     @GetMapping("/{notificationId}")
-    public Notification getNotification(@PathVariable String notificationId) {
+    public Notification getNotification(@PathVariable("notificationId") String notificationId) {
         return notificationService.getNotification(notificationId);
     }
 
     @DeleteMapping("/{notificationId}")
-    public ResponseEntity<String> deleteNotification(@PathVariable String notificationId) {
+    public ResponseEntity<String> deleteNotification(@PathVariable("notificationId") String notificationId) {
         boolean message = notificationService.removeNotification(notificationId);
 
         if (message) {
-            return ResponseEntity.ok().build();
+            return ResponseEntity.ok("Deleted notification successfully");
         } else {
             return ResponseEntity.internalServerError().build();
         }
