e47b5e878c05e5e92cd84a1b6892f9006f33d0d3
ralap_na <lydia88na@gmail.com>
2025-04-17 13:44:39 +0800

    [fix] Revised FeedbackController to send notification after creating feedbacks.

--- src/main/java/blog/feedback/controller/FeedbackController.java
+++ src/main/java/blog/feedback/controller/FeedbackController.java
@@ -5,6 +5,7 @@ import blog.common.OutcomeState;
 import blog.feedback.Comment;
 import blog.feedback.Reaction;
 import blog.feedback.service.FeedbackService;
+import blog.notification.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,11 +19,15 @@ public class FeedbackController {
     @Autowired
     FeedbackService feedbackService;
 
-    @PostMapping("/{userId}/{articleId}/comment-edited")
-    public ResponseEntity<String> createComment(@PathVariable String userId, @PathVariable String articleId, @RequestBody String content){
-        OperationOutcome outcome = feedbackService.createComment(articleId, userId, content, Instant.now());
+    @Autowired
+    NotificationService notificationService;
 
-        if(outcome.getState().equals(OutcomeState.SUCCESS)){
+    @PostMapping("/{writerId}/{articleId}/{readerId}/comment-edited")
+    public ResponseEntity<String> createComment(@PathVariable String writerId, @PathVariable String articleId, @PathVariable String readerId,@RequestBody String content){
+        OperationOutcome outcome = feedbackService.createComment(articleId, readerId, content, Instant.now());
+        boolean isNotificationSent = notificationService.notifyUser(writerId, articleId, "You have received a comment!", "", Instant.now());
+
+        if(outcome.getState().equals(OutcomeState.SUCCESS) && isNotificationSent){
             return ResponseEntity.ok().build();
         }
         else{
@@ -43,40 +48,83 @@ public class FeedbackController {
     }
 
     @GetMapping("/{commentId}")
-    public Comment getCommentById(@PathVariable("commentId") String commentId) {
-        return feedbackService.getCommentById(commentId);
+    public ResponseEntity<Comment> getCommentById(@PathVariable("commentId") String commentId) {
+        Comment comment = feedbackService.getCommentById(commentId);
+
+        if(comment != null){
+            return ResponseEntity.ok().body(comment);
+        }
+        else{
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
     @GetMapping("/{articleId}/comments")
-    public List<Comment> getNotificationByArticleId(@PathVariable("articleId") String articleId) {
-        return feedbackService.getCommentsByArticleId(articleId);
+    public ResponseEntity<List<Comment>> getCommentsByArticleId(@PathVariable("articleId") String articleId) {
+        List<Comment> comments = feedbackService.getCommentsByArticleId(articleId);
+
+        if(comments != null){
+            return ResponseEntity.ok().body(comments);
+        }
+        else{
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
     @GetMapping("/comments")
-    public List<Comment> getAllComments() {
-        return feedbackService.getAllComments();
+    public ResponseEntity<List<Comment>> getAllComments() {
+        List<Comment> comments = feedbackService.getAllComments();
+
+        if(comments != null){
+            return ResponseEntity.ok().body(comments);
+        }
+        else{
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
     @GetMapping("/{articleId}/reactions")
-    public List<Reaction> getReactionsByArticleId(@PathVariable("articleId") String articleId) {
-        return feedbackService.getReactionsByArticleId(articleId);
+    public ResponseEntity<List<Reaction>> getReactionsByArticleId(@PathVariable("articleId") String articleId) {
+        List<Reaction> reactions = feedbackService.getReactionsByArticleId(articleId);
+
+        if(reactions != null){
+            return ResponseEntity.ok().body(reactions);
+        }
+        else{
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
     @GetMapping("/reactions")
-    public List<Reaction> getAllReactions() {
-        return feedbackService.getAllReactions();
+    public ResponseEntity<List<Reaction>> getAllReactions() {
+        List<Reaction> reactions = feedbackService.getAllReactions();
+
+        if(reactions != null){
+            return ResponseEntity.ok().body(reactions);
+        }
+        else{
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
     @GetMapping("/{reactionId}")
-    public Reaction getReactionById(@PathVariable("reactionId") String reactionId) {
-        return feedbackService.getReactionById(reactionId);
+    public ResponseEntity<Reaction> getReactionById(@PathVariable("reactionId") String reactionId) {
+        Reaction reaction = feedbackService.getReactionById(reactionId);
+
+        if(reaction != null){
+            return ResponseEntity.ok().body(reaction);
+        }
+        else{
+            return ResponseEntity.internalServerError().build();
+        }
     }
 
-    @PostMapping("/{userId}/{articleId}/add-reaction")
-    public ResponseEntity<String> addReaction(@PathVariable String userId, @PathVariable String articleId, @RequestBody String type){
-        OperationOutcome outcome = feedbackService.addReaction(articleId, userId, type);
+    @PostMapping("/{writerId}/{articleId}/{readerId}/add-reaction")
+    public ResponseEntity<String> addReaction(@PathVariable String writerId, @PathVariable String articleId, @PathVariable String readerId, @RequestBody String type){
+        OperationOutcome outcome = feedbackService.addReaction(articleId, readerId, type);
+        boolean isNotificationSent = notificationService.notifyUser(writerId, articleId, "You have received a reaction!", "", Instant.now());
 
-        if(outcome.getState().equals(OutcomeState.SUCCESS)){
+        if(outcome.getState().equals(OutcomeState.SUCCESS) && isNotificationSent){
             return ResponseEntity.ok().build();
         }
         else{
